23f4e4a4bfb8f3c25f59012e2c4592d50e27fb03
Ebihara Yusuke <yusuke@ebihara.me>
2024-02-10 18:17:38 +0900

    feat: support control RTT measurement using ECHO_REQUEST
    This will be supported after Shunkei VTX v0.8.0

--- shunkei_sdk/__init__.py
+++ shunkei_sdk/__init__.py
@@ -1,16 +1,23 @@
 from __future__ import annotations
 import socket
 import time
+import queue
+import threading
+import datetime
 
 from .webrtc import WebRTCProxy
 from .find_shunkei import find_vtx_tx_first
 
 DEFAULT_PORT = 12334
 
+PACKET_TYPE_ECHO_REQUEST = 0
+PACKET_TYPE_ECHO_RESPONSE = 1
 PACKET_TYPE_UART = 128
 
 # TODO: handle iroriona error
 
+def get_timestamp_us() -> int:
+    return int(datetime.datetime.now().timestamp() * 1e6)
 class FindShunkeiError(Exception):
     pass
 
@@ -20,16 +27,28 @@ class ShunkeiVTX:
     _host: str = None
     _port: int = None
 
+    _control_rtt: int | None = None
+    _control_rtt_last: datetime.datetime | None = None
+
+    _recv_thread = None
+    _control_rtt_thread = None
+    _alive: bool = True
+    _uart_queue: queue.Queue = queue.Queue()
+
     @classmethod
     def connect_via_ip(cls, host: str, port: int) -> ShunkeiVTX:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.connect((host, port))
-        sock.setblocking(False)
 
         insatance = cls()
         insatance._socket = sock
         insatance._host = host
         insatance._port = port
+
+        insatance._recv_thread = threading.Thread(target=insatance._recv_thread_handler)
+        insatance._recv_thread.start()
+        insatance._control_rtt_thread = threading.Thread(target=insatance._rtt_thread_handler)
+        insatance._control_rtt_thread.start()
 
         return insatance
 
@@ -56,22 +75,46 @@ class ShunkeiVTX:
 
         return instance
 
-    def uart_read(self, size: int) -> bytes:
-        if self._webRTCProxy is not None and not self._webRTCProxy.alive():
-            raise Exception("webrtc proxy is stopped")
-        try:
-            buf = self._socket.recv(size)
-            if buf[0] == PACKET_TYPE_UART:
-                return buf[1:]
-            else:
-                return None
-        except BlockingIOError:
+    def uart_read(self, _) -> bytes:
+        if self._uart_queue.empty():
             return None
+        return self._uart_queue.get()
 
     def uart_write(self, data: bytes):
         if self._webRTCProxy is not None and not self._webRTCProxy.alive():
             raise Exception("webrtc proxy is stopped")
         self._socket.send(bytes([PACKET_TYPE_UART]) + data)
+
+    @property
+    def control_rtt_us(self) -> int | None:
+        """
+        return the rtt of control signal(UART) in micro seconds
+        """
+        return self._control_rtt
+
+    @property
+    def control_rtt_last(self) -> datetime.datetime | None:
+        """
+        return the time when the last control rtt was measured
+        """
+        return self._control_rtt_last
+
+    def _recv_thread_handler(self):
+        while self._alive:
+            buf = self._socket.recv(1024)
+            if buf[0] == PACKET_TYPE_UART:
+                self._uart_queue.put(buf[1:])
+            if buf[0] == PACKET_TYPE_ECHO_RESPONSE:
+                start = int.from_bytes(buf[1:], "little")
+                now = get_timestamp_us()
+                self._control_rtt = now - start
+                self._control_rtt_last = datetime.datetime.now()
+
+    def _rtt_thread_handler(self):
+        while self._alive:
+            start = get_timestamp_us()
+            self._socket.send(bytes([PACKET_TYPE_ECHO_REQUEST]) + start.to_bytes(16, "little"))
+            time.sleep(1)
 
     @property
     def host(self) -> str:
@@ -82,7 +125,14 @@ class ShunkeiVTX:
         return self._port
 
     def close(self):
+        self._alive = False
+        if self._recv_thread:
+            self._recv_thread.join()
+        if self._control_rtt_thread:
+            self._control_rtt_thread.join()
+
         self._socket.close()
+
         if self._webRTCProxy is not None:
             self._webRTCProxy.stop()
 
